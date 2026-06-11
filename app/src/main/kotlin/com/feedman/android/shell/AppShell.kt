86bb51f2e28945c3a33fe90e9263203ff7aa7bd8
Hitoshi Ichikawa <hitoshi@goodanswers.inc>
2026-06-12 08:59:05 +0900

    fix(shell): AppShell.kt の padding import を追加してコンパイルを通す

--- app/src/main/kotlin/com/feedman/android/shell/AppShell.kt
+++ app/src/main/kotlin/com/feedman/android/shell/AppShell.kt
@@ -1,5 +1,6 @@
 package com.feedman.android.shell
 
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ModalNavigationDrawer
